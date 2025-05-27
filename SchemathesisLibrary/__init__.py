0f3565943f5a59b95b68ed7cf01bf7541c4c4097
Tatu Aalto <aalto.tatu@gmail.com>
2025-05-27 22:39:49 +0300

    feat: fist keyword and initial inpmlementation

--- SchemathesisLibrary/__init__.py
+++ SchemathesisLibrary/__init__.py
@@ -11,38 +11,44 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from dataclasses import dataclass
+from typing import Any
+
 import schemathesis
-from hypothesis import given, settings, HealthCheck, Phase, Verbosity
 from DataDriver import DataDriver  # type: ignore
-from robot.api.deco import keyword
-from robot.running.model import TestCase, TestSuite  # type: ignore
-from robot.result.model import TestSuite as ResultTestSuite  # type: ignore
-from robot.result.model import TestCase as ResultTestCase  # type: ignore
-from robotlibcore import DynamicCore  # type: ignore
 from DataDriver.AbstractReaderClass import AbstractReaderClass  # type: ignore
 from DataDriver.ReaderConfig import TestCaseData  # type: ignore
+from hypothesis import HealthCheck, Phase, Verbosity, given, settings
+from hypothesis import strategies as st
+from robot.api.deco import keyword
+from robot.result.model import TestCase as ResultTestCase  # type: ignore
+from robot.result.model import TestSuite as ResultTestSuite  # type: ignore
+from robot.running.model import TestCase, TestSuite  # type: ignore
+from robotlibcore import DynamicCore  # type: ignore
 
 __version__ = "0.1.0"
 
 
+@dataclass
+class Options:
+    url: "str|None" = None
+
+
 class SchemathesisReader(AbstractReaderClass):
-    def __init__(self, *, url: "str|None" = None):
-        self.url = url
+    options: "Options|None" = None
 
     def get_data_from_source(
         self,
-    ):  # This method will be called from DataDriver to get the TestCaseData list.
-        schema = schemathesis.from_uri(self.url)
+    ) -> list[TestCaseData]:
+        schema = schemathesis.from_uri(self.options.url)  # type: ignore
         all_cases = []
         for op in schema.get_all_operations():
-            op_as_strategy = op.ok().as_strategy()
+            op_as_strategy = op.ok().as_strategy()  # type: ignore
             for case in generate_examples(op_as_strategy, 10):
                 args = {
                     "${case}": case,
                 }
-                all_cases.append(
-                    TestCaseData(test_case_name=str(case.id), arguments=args)
-                )
+                all_cases.append(TestCaseData(test_case_name=str(case.id), arguments=args))
         return all_cases
 
 
@@ -51,17 +57,16 @@ class SchemathesisLibrary(DynamicCore):
     ROBOT_LISTENER_API_VERSION = 3
     ROBOT_LIBRARY_SCOPE = "TEST SUITE"
 
-    def __init__(self, *, url: "str|None" = None):
+    def __init__(self, *, url: "str|None" = None) -> None:
         self.ROBOT_LIBRARY_LISTENER = self
-        self.data_driver = DataDriver(
-            reader_class=SchemathesisReader(url=url), file_regex=""
-        )
+        SchemathesisReader.options = Options(url=url)
+        self.data_driver = DataDriver(reader_class=SchemathesisReader)
         DynamicCore.__init__(self, [])
 
-    def _start_suite(self, data: TestSuite, result: ResultTestSuite):
+    def _start_suite(self, data: TestSuite, result: ResultTestSuite) -> None:
         self.data_driver._start_suite(data, result)
 
-    def _start_test(self, data: TestCase, result: ResultTestCase):
+    def _start_test(self, data: TestCase, result: ResultTestCase) -> None:
         self.data_driver._start_test(data, result)
 
     @keyword
@@ -70,7 +75,7 @@ class SchemathesisLibrary(DynamicCore):
         case.call_and_validate()
 
 
-def generate_examples(strategy, number) -> list[schemathesis.Case]:
+def generate_examples(strategy: st.SearchStrategy, number: int) -> list[schemathesis.Case]:
     examples = []
 
     @given(strategy)
@@ -82,7 +87,7 @@ def generate_examples(strategy, number) -> list[schemathesis.Case]:
         phases=(Phase.generate,),
         suppress_health_check=list(HealthCheck),
     )
-    def example_generating_inner_function(ex):
+    def example_generating_inner_function(ex: Any) -> None:
         examples.append(ex)
 
     example_generating_inner_function()
