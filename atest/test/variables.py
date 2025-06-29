de4eec81cd92e86c8b271950bed8dda2d222df19
Tatu Aalto <aalto.tatu@gmail.com>
2025-06-29 23:14:00 +0300

    First release

--- atest/test/variables.py
+++ atest/test/variables.py
@@ -4,5 +4,5 @@ from pathlib import Path
 def get_variables():
     return {
         "LIBRARY_OUTPUT_DIR_BASE": str(Path(__file__).parent.parent / "output_library"),
-        "LIBRARY_PYTHON_PATH": str(Path(__file__).parent.parent.parent),
+        "LIBRARY_PYTHON_PATH": str(Path(__file__).parent.parent.parent / "src"),
     }
