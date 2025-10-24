1fd7ad223b4ef3a5a838fc4aac6ccef27574b7d3
Schemathesis Library <Schemathesis@Library>
2025-10-24 04:03:34 +0000

    Update version information to 0.49.0

--- src/SchemathesisLibrary/__init__.py
+++ src/SchemathesisLibrary/__init__.py
@@ -31,7 +31,7 @@ from schemathesis.core import NotSet
 from schemathesis.core.result import Ok
 from schemathesis.core.transport import Response
 
-__version__ = "0.48.0"
+__version__ = "0.49.0"
 
 
 @dataclass
